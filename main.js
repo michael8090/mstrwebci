3e1209284713f5b55a9d9b3f4fc5cf1cab935e86
michael <michael8090@gmail.com>
2014-09-08 23:52:18 +0800

    restructure

--- main.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by yohuang on 9/5/2014.
- */
-(function () {
-    exports.build = require('./builder').build;
-})();
--- setupEnv.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by yohuang on 9/4/2014.
- */
-(function () {
-    var utils = require('./utils');
-    global.projectRoot = utils.findProjectRoot();
-})();
